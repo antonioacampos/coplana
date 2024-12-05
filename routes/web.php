e3dd6d96a75a4ab86b4b0e23fcc4632defa2ad26
antonioacampos <antonioa.campos64@gmail.com>
2024-12-05 19:37:04 +0000

    server firsts tests

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InterfaceController;
 
-Route::get('/', [InterfaceController::Class, 'home']);
+Route::get('/coplana', [InterfaceController::Class, 'home']);
 
-Route::get('/calculadora/{cropType}', [InterfaceController::class, 'index'])->name('calculadora');
-Route::post('/secoes/{cropType}', [InterfaceController::class, 'getInputs'])->name('get.inputs');
+Route::get('coplana/calculadora/{cropType}', [InterfaceController::class, 'index'])->name('calculadora');
+Route::post('coplana/secoes/{cropType}', [InterfaceController::class, 'getInputs'])->name('get.inputs');
 
-Route::post('/calcular', [InterfaceController::class, 'calcular'])->name('calcular');
+Route::post('coplana/calcular', [InterfaceController::class, 'calcular'])->name('calcular');
 
-Route::get('/export/csv', [InterfaceController::class, 'exportCsv'])->name('export.csv');
-Route::get('/export/pdf', [InterfaceController::class, 'exportPdf'])->name('export.pdf');
+Route::get('coplana/export/csv', [InterfaceController::class, 'exportCsv'])->name('export.csv');
+Route::get('coplana/export/pdf', [InterfaceController::class, 'exportPdf'])->name('export.pdf');
 
 Route::fallback(function(){
     return view('errors.404');
